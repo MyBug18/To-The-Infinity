c27d36baa0fe42024bb03d94f01e8fefce0bb465
MyBug18 <mskimkim0318@gmail.com>
2020-09-11 15:59:11 +0900

    Added job and group info to PopSlotData

--- Assets/Scripts/Core/GameData/PopSlotData.cs
+++ Assets/Scripts/Core/GameData/PopSlotData.cs
@@ -10,6 +10,10 @@ namespace Core.GameData
 
         public bool HasDefaultValue => _default != null;
 
+        public HashSet<string> AllJobName { get; } =  new HashSet<string>();
+
+        public Dictionary<string, List<string>> AllGroupInfo { get; } = new Dictionary<string, List<string>>();
+
         public void AddNewData(ILuaHolder luaHolder)
         {
             if (!(luaHolder is PopSlotPrototype psp)) return;
@@ -23,6 +27,12 @@ namespace Core.GameData
             }
 
             _data[psp.Name] = psp;
+            AllJobName.Add(psp.Name);
+
+            if (!AllGroupInfo.ContainsKey(psp.Group))
+                AllGroupInfo.Add(psp.Group, new List<string>());
+
+            AllGroupInfo[psp.Group].Add(psp.Name);
         }
 
         public void OnGameInitialized(Game _)
