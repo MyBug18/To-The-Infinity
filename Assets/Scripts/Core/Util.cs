1c3ed7a960b82e9d60221fb56e48bda28ca1bc94
MyBug18 <mskimkim0318@gmail.com>
2020-09-11 16:56:32 +0900

    Added Util class

---
+++ Assets/Scripts/Core/Util.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public static class Util
+    {
+        public static TValue TryGetKeyWithDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key,
+            TValue defaultValue) => dict.TryGetValue(key, out var result) ? result : defaultValue;
+    }
+}
