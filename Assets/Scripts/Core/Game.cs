c84f20d5fdccbb610fa76be5efccdfdf653901f0
MyBug18 <mskimkim0318@gmail.com>
2020-10-12 10:59:38 +0900

    Added IsEnemy

--- Assets/Scripts/Core/Game.cs
+++ Assets/Scripts/Core/Game.cs
@@ -4,12 +4,18 @@ namespace Core
 {
     public sealed class Game
     {
+        public static Game Instance { get; private set; }
+
         public int GameSpeed { get; }
 
         public IReadOnlyList<StarSystem> StarSystems { get; }
 
+        private HashSet<string> _enemyOwners = new HashSet<string>();
+
         public Game(int gameSpeed)
         {
+            Instance = this;
+
             GameSpeed = gameSpeed;
         }
 
@@ -19,6 +25,22 @@ namespace Core
             {
                 s.StartNewTurn(GameSpeed);
             }
+        }
+
+        public bool IsEnemy(string owner1, string owner2)
+        {
+            if (owner1 == owner2) return false;
+
+            // MainEnemy can not be friendly with any other owner
+            if (owner1 == "MainEnemy" || owner2 == "MainEnemy") return true;
+
+            if (owner1 == "Me")
+                return _enemyOwners.Contains(owner2);
+
+            if (owner2 == "Me")
+                return _enemyOwners.Contains(owner1);
+
+            return false;
         }
     }
 }
