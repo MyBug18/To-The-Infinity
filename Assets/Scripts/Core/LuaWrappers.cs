2ff1cf7a6fde2eea1612dbe91c2d2bbcad78fca0
MyBug18 <mskimkim0318@gmail.com>
2020-11-05 18:52:52 +0900

    Added LuaDoubleValueWrapper

---
+++ Assets/Scripts/Core/LuaWrappers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using MoonSharp.Interpreter;
+
+namespace Core
+{
+    /// <summary>
+    ///     Simple dictionary wrapper for lua
+    /// </summary>
+    [MoonSharpUserData]
+    public class LuaDictWrapper
+    {
+        private readonly Dictionary<string, object> _v;
+
+        public LuaDictWrapper(Dictionary<string, object> v) => _v = v;
+
+        public object Get(string key, object defaultValue) =>
+            _v.TryGetValue(key, out var result) ? result : defaultValue;
+
+        public void Set(string key, object value)
+        {
+            if (!value.GetType().IsValueType || value.GetType() != typeof(string)) return;
+
+            _v[key] = value;
+        }
+    }
+
+    /// <summary>
+    ///     Simple double value wrapper for lua
+    /// </summary>
+    [MoonSharpUserData]
+    public class LuaDoubleValueWrapper
+    {
+        private double _value;
+
+        public LuaDoubleValueWrapper(double value)
+        {
+            _value = value;
+        }
+
+        public object Get() => _value;
+
+        public void Set(double value)
+        {
+            _value = value;
+        }
+    }
+}
